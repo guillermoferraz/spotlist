99f4a071c54c23fde4b3975e3999aabe0a7962bc
Guillermo Ferraz <g.ferraz@akimad.com>
2022-08-08 20:05:24 -0300

    fix

--- api/src/loaders/express.ts
+++ api/src/loaders/express.ts
@@ -4,11 +4,10 @@ import cors from "cors";
 import morgan from "morgan";
 
 export default ({ app }: { app: express.Application }) => {
-  const port = process.env.PORT || 8000;
-  
+  const port = process.env.PORT || 9000;
+
   app.use(morgan("dev"));
   app.use(bodyParser.json());
   app.use(cors());
   app.set("port", port);
 };
-
