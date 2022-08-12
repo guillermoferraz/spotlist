ea48e2f14080780721b51a2f4346c219b9316a50
Guillermo Ferraz <g.ferraz@akimad.com>
2022-08-12 16:36:33 -0300

    change on tet

--- api/tests/api.spec.ts
+++ api/tests/api.spec.ts
@@ -3,10 +3,6 @@ import app from '../src/app';
 
 describe('GET /ping', () => {
     it('should return 200 OK', () => {
-        return request(app).get('/ping').expect(200)
-    });
-
-    it('should return `pong` in response', () => {
         return request(app).get('/ping').expect('pong')
     });
 });
