1b725acd5a4085bf79521cc476aa7ffcba15d6ab
Guillermo Ferraz <g.ferraz@akimad.com>
2022-08-12 16:27:01 -0300

    responsive for mobile is added

--- webapp/src/ui/Home/Home.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Home } from "./Home";
-
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => (jest.fn())
-}));
